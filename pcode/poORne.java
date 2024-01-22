258887efac4aee1463574dc3502b4709c248dca3
Ankitaabb <157209507+Ankitaabb@users.noreply.github.com>
2024-01-22 16:13:56 +0530

    1 push

---
+++ pcode/poORne.java
@@ -0,0 +1,16 @@
+package pcode;
+
+import java.util.Scanner;
+
+class pORne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        if (num < 0) {
+            System.out.println("the number is negative");
+        } else {
+            System.out.println("the number is positive");
+        }
+    }
+}
